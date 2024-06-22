ceb6d12832ac6bf18de1cd0db19d35d0c663820f
Aakarsh <aakarshdwivedi43@gmail.com>
2024-06-22 22:00:39 +0530

    hollowleftpyramid

---
+++ hollowleftpyramid.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=1;row<n;row++)
+    {
+            if(row==1 || row==2 || row==n-1)
+            {
+                for(int col=1;col<=row;col++){
+                cout<<"*";
+                }
+            }
+            else
+            {
+                for(int col=0;col<n;col++)
+                {
+                    if(col==0 || col==n-1)
+                    {
+                        cout<<"*";
+                    }
+                    else
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }
+}
