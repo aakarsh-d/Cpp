8ee75f226401b11af37c770c4048553534169d55
Aakarsh <aakarshdwivedi43@gmail.com>
2024-06-26 22:03:42 +0530

    fullhollowpyramid

--- fullhollowpyramid.cpp
+++ fullhollowpyramid.cpp
@@ -4,28 +4,28 @@
   {
     int n;
     cin>>n;
-    for(int row=0;row<n;row++)
+    for(int row=1;row<=n;row++)
     {
         for(int col=0;col<n-row;col++)
         {
             cout<<" ";
         }
-        if(row==0 || row==1 || row==n-1)
+        if(row==0 || row==1 || row==n)
         {
         for(int col=0;col<row;col++)
         {
-            cout<<"*";
+            cout<<"* ";
         }
         }
         else{
-            for(int col=0;col<=row;col++){
-            if(col==0 || col==n-row)
+            for(int col=0;col<=n;col++){
+            if(col==0 || col==row)
             {
                 cout<<"*";
             }
             else
             {
-                cout<<" ";
+                cout<<"  ";
             }
             }
         }
