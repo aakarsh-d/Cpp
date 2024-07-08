b79735f387ba688a32417cdbb5adff46ad0883e0
Aakarsh <aakarshdwivedi43@gmail.com>
2024-07-08 21:37:28 +0530

    STL-Deque

--- STL-Deque.cpp
+++ STL-Deque.cpp
@@ -23,13 +23,22 @@ int main()
         cout<<i<< " ";
     }
     deque<int> d;
-    d.push_back(2);
-    d.push_back(7);
-    d.push_back(3);
-    d.push_back(5);
+    d.push_back(22);
+    d.push_back(73);
+    d.push_back(36);
+    d.push_back(52);
     for(int i:d)
     {
         cout<<i<< " ";
     }
-
+    cout<<endl;
+    deque<int> dd;
+    dd.push_back(2);
+    dd.push_back(7);
+    dd.push_back(3);
+    dd.push_back(5);
+    for(int i:dd)
+    {
+        cout<<i<< " ";
+    }
 }
